371996f952edd88268f802b8fd60d9b8f52667bf
Miguel <iamcodefoxx@gmail.com>
2022-06-09 08:41:01 -0700

    update

---
+++ public/js/tiny-slider-1-manual.js
@@ -0,0 +1,14 @@
+let slider_1 = tns({
+  container: '.slider-1',
+  autoWidth: true,
+  gutter: 15,
+  animateIn: "jello",
+  animateOut: "rollOut",
+  slideBy: 1,
+  speed: 400,
+  nav: false,
+  swipeAngle: false,
+  controlsContainer: '#controls-1',
+  prevButton: '.prev-1',
+  nextButton: '.next-1'
+});
---
+++ public/js/tiny-slider-2-manual.js
@@ -0,0 +1,14 @@
+let slider_2 = tns({
+  container: '.slider-2',
+  autoWidth: true,
+  gutter: 15,
+  animateIn: "jello",
+  animateOut: "rollOut",
+  slideBy: 1,
+  speed: 400,
+  nav: false,
+  swipeAngle: false,
+  controlsContainer: '#controls-2',
+  prevButton: '.prev-2',
+  nextButton: '.next-2'
+});
