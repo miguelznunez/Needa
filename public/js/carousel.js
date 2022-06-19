8fde768dc034d31a0cd04a8e5d4cff63273585d9
Miguel <iamcodefoxx@gmail.com>
2022-06-18 19:45:28 -0700

    update

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,18 +1,23 @@
- const images = ["/images/carpenter.webp", "/images/barber.webp", "/images/djs.webp", "/images/lessons.webp"],
+ const webpImages = ["/images/carpenter.webp", "/images/barber.webp", "/images/djs.webp", "/images/lessons.webp"],
+  pngImages = ["/images/carpenter.png", "/images/barber.png", "/images/djs.png", "/images/lessons.jpg"],
   carousel = document.querySelector("#carousel"),
   submitBtn = document.querySelector("#submitBtn"),
   interval = setInterval(function () {
     startCarousel();
   }, 8000);
 
-  let index = 1;
+  let index1 = 1;
+  let index2 = 1;
 
   // CAROUSEL
 
   startCarousel = () => {
-    carousel.style.backgroundImage = `linear-gradient(0deg, rgba(0,0,0,0.7), rgba(0, 0, 0, 0.7)), url(${images[index++]})`;
+    carousel.style.backgroundImage = `url(${pngImages[index1++]}), url(${webpImages[index2++]})`;
     carousel.classList.remove("fade");
     void carousel.offsetWidth;
     carousel.classList.add("fade");
-    if (index > images.length - 1) index = 0;
+    if (index1 > webpImages.length - 1) {
+      index1 = 0;
+      index2 = 0;
+    }
   }
