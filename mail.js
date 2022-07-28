cd6032ccecf17cf60d99675daac8dce6a1780daa
Miguel <iamcodefoxx@gmail.com>
2022-07-28 12:31:59 -0700

    update

--- mail.js
+++ mail.js
@@ -18,7 +18,7 @@ activateAccountEmail = (email, id, token, cb) => {
       from: "mignunez@csumb.edu",
       to: email,
       subject: "Account activation",
-      html: `<p>Please click on the following link, or paste it into your browser to complete the account activation process:<br><br><a href="https://code-foxx.com/activate-account/${id}/${token}">https://code-foxx.com/activate-account/${id}/${token}</a><br><br>The Team</p>`
+      html: `<p>Please click on the following link, or paste it into your browser to complete the account activation process:<br><br><a href="http://www.needa.online/activate-account/${id}/${token}">http://www.needa.online/activate-account/${id}/${token}</a><br><br>The Team</p>`
   };
   transporter.sendMail(mailOptions, function(err, data) {
     if (err) cb(err, null);
@@ -32,7 +32,7 @@ resetPasswordEmail = (email, id, token, cb) => {
       from: "mignunez@csumb.edu",
       to: email,
       subject: "Password reset",
-      html: `<p>You are receiving this because you (or someone else) have requested the reset of the password for your account. Please click on the following link, or paste it into your browser to complete the process within one hour of receiving it:<br><br><a href="https://code-foxx.com/password-reset-update/${id}/${token}">https://code-foxx.com/password-reset-update/${id}/${token}</a><br><br>If you did not request this, please ignore this email and your password will remain unchanged.<br><br>The Team</p>`
+      html: `<p>You are receiving this because you (or someone else) have requested the reset of the password for your account. Please click on the following link, or paste it into your browser to complete the process within one hour of receiving it:<br><br><a href="http://www.needa.online/password-reset-update/${id}/${token}">http://www.needa.online/password-reset-update/${id}/${token}</a><br><br>If you did not request this, please ignore this email and your password will remain unchanged.<br><br>The Team</p>`
   };
   transporter.sendMail(mailOptions, function(err, data) {
     if (err) cb(err, null);
