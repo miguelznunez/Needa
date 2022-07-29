96f838018ff6bd75dbc54885145c859f90fd9588
Miguel <iamcodefoxx@gmail.com>
2022-07-28 17:21:42 -0700

    update

--- public/js/load-our-testimonials.js
+++ public/js/load-our-testimonials.js
@@ -5,25 +5,25 @@ let tObject = [
     "img": "images/landscaper.jpg",
     "name" : "Earl Stevens",
     "profession" : "Landscaper",
-    "userId" : "3"
+    "userId" : "2"
   },
   {
     "img": "images/tutor.jpg",
     "name" : "Madison Lee",
     "profession" : "Tutor",
-    "userId" : "4"
+    "userId" : "3"
   },
   {
     "img": "images/disc_jockey.jpg",
-    "name" : "Anthony Altayeb",
+    "name" : "Anthony Carter",
     "profession" : "Disc Jockey",
-    "userId" : "5"
+    "userId" : "3"
   },
   {
     "img": "images/personal_trainer.jpg",
     "name" : "Jason Corsa",
     "profession" : "Personal Trainer",
-    "userId" : "6"
+    "userId" : "4"
   }
 ].sort( () => Math.random() - 0.5);
 
@@ -51,7 +51,7 @@ function initializeTestimonials(){
                         <span class="border"></span>
                       </div>
                       <div class="testimonials-box-user">
-                      <a href="http://localhost:5000/search-results-user-profile/${tObject[testimonial].userId}"><button>View User</button></a>
+                      <a href="http://needa.online/search-results-user-profile/${tObject[testimonial].userId}"><button>View User</button></a>
                         <div class="flex-row-gap-20">
                           <i class="fa-solid fa-phone-flip"></i>
                           <i class="fa-solid fa-envelope"></i>
